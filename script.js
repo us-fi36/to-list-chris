981b7859315a577856ea3b0bac0b3b295ecad8ee
Chris <chris.herfort@gmx.eu>
2024-09-27 12:18:57 +0000

    Del get json

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-function loadTodos() {
-    fetch('index.php')
-    .then(Response => Response.json())
-    .then(todos => {
-        const todoList =
-        document.getElementById('todoList');
-        todoList.innerHTML = '';
-        todos.forEach(todo => {
-            const li = 
-            document.createElement('li');
-            li.textContent = todo;
-            todoList.appendChild(li);
-        })
-    })
-}
-window.onload = loadTodos;
-
-document.getElementById('todoForm').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const todoInput = document.getElementById('todoInput').value;
-    fetch('index.php', {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({todo: todoInput})
-    })
-    
-    .then (response => response.json ())
-    .then((data) =>{
-    console.log(data)
-    loadTodos();
-    document.getElementById('todoInput').value ='';
-    });
-});
-
 // Auswahl der HTML-Elemente
 const todoForm = document.getElementById('todoForm');
 const todoInput = document.getElementById('todoInput');
