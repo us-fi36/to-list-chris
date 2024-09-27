6f72528d037abed2e84516a83f88b0dea924d8da
Chris <chris.herfort@gmx.eu>
2024-09-27 15:02:21 +0200

    del js err

--- neu/script.js
+++ neu/script.js
@@ -1,4 +1,4 @@
-document. addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 
     // Define the URL to our PHP API.
     const apiUrl = "todo-api.php";
