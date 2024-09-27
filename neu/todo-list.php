33cf89dedcaed966da437c9730a59df6d65e9e56
Chris <chris.herfort@gmx.eu>
2024-09-27 15:15:46 +0200

    update php

--- neu/todo-list.php
+++ neu/todo-list.php
@@ -1,30 +1,32 @@
-<? php
-
-//.LOG.function in .PHP
-function write_log ($action, $data) {
-    $log =fopen('log.txt', 'a');
-    $timestamp = date('Y-m-d.H:i:s');
-    fwrite($log, "$timestamp - $action :. " .. json_encode($data) .. "\n");
-    fclose($log);    
-}
+<?php
 
 header("Content-Type: application/json");
-switch ($_SERVER["REQUEST_METHOD"] ) {
-case "GET":
-//Get Todo's (READ)
-.write_log("READ", null);
-break;
-case "POST":
-//Add Todo (CREATE)
-write_log("CREATE", null);
-break;
-case "PUT":
-//Change Todo (UPDATE)
-write_log("PUT", null);
-break;
-case "DELETE":
-//Remove Todo (DELETE)
-write_log("DELETE", null);
-break;
+
+// LOG function in PHP
+function write_log($action, $data) {
+    $log = fopen('log.txt', 'a');
+    $timestamp = date('Y-m-d H:i:s');
+    fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
+    fclose($log);
 }
-?> 
+
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+        // Get Todo's (READ)
+        write_log("READ", null);
+        break;
+    case "POST":
+        // Add Todo (CREATE)
+        write_log("CREATE", null);
+        break;
+    case "PUT":
+        // Change Todo (UPDATE)
+        write_log("PUT", null);
+        break;
+    case "DELETE":
+        // Remove Todo (DELETE)
+        write_log("DELETE", null);
+        break;
+}
+
+?>
